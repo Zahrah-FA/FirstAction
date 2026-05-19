15bfac0e8defe424a59993a533dced5ce3786359
Zahrah-FA <zafa3005@gmail.com>
2026-05-19 19:04:49 +0700

    Pembetulan Inisialisasi bab 1

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>FirstAction</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#E63946', 
+    marginBottom: 5,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#666',
   },
 });
